56389cd3e44e5812a17794a0f7e21ab8c6f57984
Razeto <gcardenas1720@gmail.com>
2021-09-13 19:41:27 -0500

    Diseño web-home

--- GobiernoDigitalApp/urls.py
+++ GobiernoDigitalApp/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.home,name="Home"),
+    path('addproject',views.addproject,name="AddProject"),
 ]
 
-#urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- Proyectos/views.py
+++ Proyectos/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def proyectos(request):
+    return render(request,"proyectos.html")
