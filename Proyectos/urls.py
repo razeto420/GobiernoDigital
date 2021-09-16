a1c9c317fd2b15ff35cfe32803ea5534db2f425b
Aldahid <72262732@continental.edu.pe>
2021-09-16 15:12:26 -0500

    logion y registro
    Hice cambio en la plantilla de login y registro

--- Proyectos/urls.py
+++ Proyectos/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('proyectos/',views.proyectos,name="Proyectos"),
     path('login/',views.login,name="login"),
+    path('registro/',views.registro,name="registro"),
     
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Proyectos/views.py
+++ Proyectos/views.py
@@ -6,3 +6,6 @@ def proyectos(request):
 
 def login(request):
     return render(request,"login.html")
+
+def registro(request):
+    return render(request,"registro.html")
