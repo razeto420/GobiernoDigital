f19c0c53ab124fe62fa89e62b959888829a6ad04
Carlos <ctaipecasas@gmail.com>
2021-10-01 17:01:47 -0500

    Prueba

--- GobiernoDigital/settings.py
+++ GobiernoDigital/settings.py
@@ -133,6 +133,17 @@ AUTHENTICATION_BACKENDS=(
     'allauth.account.auth_backends.AuthenticationBackend',
 )
 
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
 
 STATIC_URL = '/static/'
 MEDIA_URL='/media/'
